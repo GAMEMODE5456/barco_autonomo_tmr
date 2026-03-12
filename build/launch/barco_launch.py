1cce4b16018e6de4962777447dbaae33e16838ae
GAMEMODE5456 <lucasrosasdiego@gmail.com>
2026-03-11 21:05:01 -0600

    se agrego carpeta launch/ para arrancar los 5 nodos principales al mismo tiempo

---
+++ build/launch/barco_launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='barco_autonomo_tmr',
+            executable='gps_node',
+            name='gps_node'
+        ),
+        Node(
+            package='barco_autonomo_tmr',
+            executable='imu_node',
+            name='imu_node'
+        ),
+        Node(
+            package='barco_autonomo_tmr',
+            executable='navigation_node',
+            name='navigation_node'
+        ),
+        Node(
+            package='barco_autonomo_tmr',
+            executable='motor_node',
+            name='motor_node'
+        ),
+        Node(
+            package='barco_autonomo_tmr',
+            executable='servo_node',
+            name='servo_node'
+        ),
+    ])
